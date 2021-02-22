940817a700de0d30cbac52dc0657717ce5e5eed4
andy baer <andy.baer@gmail.com>
2021-02-22 11:33:46 +0100

    test cases

--- src/test/java/org/mterra/mpc/MpcUtilsTest.java
+++ src/test/java/org/mterra/mpc/MpcUtilsTest.java
@@ -2,6 +2,7 @@ package org.mterra.mpc;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -13,21 +14,35 @@ import java.nio.file.Path;
 
 public class MpcUtilsTest {
 
-    private File resultDir = new File("./target/result");
+    private final File resultDir = new File("./target/result");
 
-    @Test
-    public void reorderSeqs() throws Exception {
+    @BeforeEach
+    public void prepare() throws Exception {
         FileUtils.deleteDirectory(resultDir);
         resultDir.mkdirs();
-        String[] args = new String[]{"./src/test/resources", "./target/result"};
-        MpcUtils.main(args);
-        assertFileContent("1.sxq", "1");
-        assertFileContent("20.sxq", "20");
-        assertFileContent("5.sxq", "4");
     }
 
-    private void assertFileContent(String filename, String expectedContent) throws IOException {
-        byte[] bytes = Files.readAllBytes(Path.of("target", "result", "Aerial_[ProjectData]", filename));
+    @Test
+    public void withAir() throws Exception {
+
+        String[] args = new String[]{"./src/test/resources/Air", "./target/result"};
+        MpcUtils.main(args);
+        assertFileContent("Aerial", "1.sxq", "1");
+        assertFileContent("Aerial", "20.sxq", "20");
+        assertFileContent("Aerial", "5.sxq", "4");
+    }
+
+    @Test
+    public void noAir() throws Exception {
+        String[] args = new String[]{"./src/test/resources/NoAir", "./target/result"};
+        MpcUtils.main(args);
+        assertFileContent("NoAir", "1.sxq", "1");
+        assertFileContent("NoAir", "20.sxq", "20");
+        assertFileContent("NoAir", "5.sxq", "4");
+    }
+
+    private void assertFileContent(String projDirPrefix, String filename, String expectedContent) throws IOException {
+        byte[] bytes = Files.readAllBytes(Path.of("target", "result", projDirPrefix + "_[ProjectData]", filename));
         String content = new String(bytes, StandardCharsets.UTF_8);
         Assertions.assertEquals(expectedContent, content);
     }
